adf3a530d9193e14003529258431cd0d02ef85d8
pecolynx <pecolynx@gmail.com>
2023-12-18 00:14:38 +0900

    develop

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ https://github.com/bazelbuild/bazelisk
 ```
 GOPROXY=direct go install github.com/pecolynx/pkgforbid/cmd/pkgforbid@latest
 ```
+
+lcov
---
+++ cocotola-api/src/app/service/service_test.go
@@ -0,0 +1,11 @@
+package service_test
+
+import (
+	"testing"
+
+	"github.com/kujilabo/cocotola-1.21/cocotola-api/src/app/service"
+)
+
+func TestA(t *testing.T) {
+	service.A()
+}
