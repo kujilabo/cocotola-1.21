63d212f21214f0c7ccd1e34c237ece873232a26a
pecolynx <pecolynx@gmail.com>
2023-12-24 01:32:14 +0900

    develop

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -39,3 +39,23 @@ golangci-lint run --disable-all --config ../.github/.golangci.yml \
 -E whitespace && \
 pkgforbid -config=../.github/pkgforbid.yml ./... && \
 popd
+
+pushd lib
+golangci-lint run --config ../.github/.golangci.yml && \
+golangci-lint run --disable-all --config ../.github/.golangci.yml \
+-E bodyclose \
+-E errorlint \
+-E exhaustive \
+-E forbidigo \
+-E forcetypeassert \
+-E gocognit \
+-E gocyclo \
+-E gofmt \
+-E goimports \
+-E gomnd \
+-E gosec \
+-E noctx \
+-E unconvert \
+-E whitespace && \
+pkgforbid -config=../.github/pkgforbid.yml ./... && \
+popd
