fa15708c7b46ea210fec9b7e415b6c5515143fd1
pecolynx <pecolynx@gmail.com>
2023-12-23 23:53:28 +0900

    develop

--- cocotola-auth/go.mod
+++ cocotola-auth/go.mod
@@ -3,9 +3,15 @@ module github.com/kujilabo/cocotola-1.21/cocotola-auth
 go 1.21.4
 
 require (
+	github.com/gin-contrib/cors v1.5.0
 	github.com/gin-gonic/gin v1.9.1
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/kujilabo/cocotola-1.21/lib v0.0.0-20231223144514-1499f74c326f
 	github.com/kujilabo/redstart v0.0.3
+	github.com/samber/slog-gin v1.7.1
+	go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin v0.46.1
 	go.opentelemetry.io/otel v1.21.0
+	go.opentelemetry.io/otel/trace v1.21.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
@@ -17,13 +23,14 @@ require (
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/internal/resourcemapping v0.45.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bytedance/sonic v1.10.2 // indirect
+	github.com/casbin/casbin/v2 v2.80.0 // indirect
+	github.com/casbin/govaluate v1.1.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
-	github.com/gin-contrib/cors v1.5.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -36,6 +43,7 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/google/uuid v1.5.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.1 // indirect
@@ -67,7 +75,6 @@ require (
 	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.21.0 // indirect
 	go.opentelemetry.io/otel/metric v1.21.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.21.0 // indirect
-	go.opentelemetry.io/otel/trace v1.21.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/arch v0.6.0 // indirect
--- cocotola-auth/src/controller/gin/auth_handler.go
+++ cocotola-auth/src/controller/gin/auth_handler.go
@@ -1,44 +1,44 @@
 package handler
 
-// import (
-// 	"github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
 
-// 	"github.com/kujilabo/cocotola-1.21/cocotola-auth/src/service"
-// 	liblog "github.com/kujilabo/cocotola-1.21/lib/log"
-// 	rsliblog "github.com/kujilabo/redstart/lib/log"
-// )
+	"github.com/kujilabo/cocotola-1.21/cocotola-auth/src/service"
+	liblog "github.com/kujilabo/cocotola-1.21/lib/log"
+	rsliblog "github.com/kujilabo/redstart/lib/log"
+)
 
-// type AuthHandler interface {
-// 	RefreshToken(c *gin.Context)
-// }
+type AuthHandler interface {
+	RefreshToken(c *gin.Context)
+}
 
-// type authHandler struct {
-// 	authTokenManager service.AuthTokenManager
-// }
+type authHandler struct {
+	authTokenManager service.AuthTokenManager
+}
 
-// func NewAuthHandler(authTokenManager service.AuthTokenManager) AuthHandler {
-// 	return &authHandler{
-// 		authTokenManager: authTokenManager,
-// 	}
-// }
+func NewAuthHandler(authTokenManager service.AuthTokenManager) AuthHandler {
+	return &authHandler{
+		authTokenManager: authTokenManager,
+	}
+}
 
-// func (h *authHandler) RefreshToken(c *gin.Context) {
-// 	ctx := c.Request.Context()
-// 	logger := rsliblog.GetLoggerFromContext(ctx, liblog.AppControllerLoggerContextKey)
-// 	logger.Info("Authorize")
-// 	refreshTokenParameter := RefreshTokenParameter{}
-// 	if err := c.BindJSON(&refreshTokenParameter); err != nil {
-// 		return
-// 	}
+func (h *authHandler) RefreshToken(c *gin.Context) {
+	ctx := c.Request.Context()
+	logger := rsliblog.GetLoggerFromContext(ctx, liblog.AppControllerLoggerContextKey)
+	logger.Info("Authorize")
+	refreshTokenParameter := RefreshTokenParameter{}
+	if err := c.BindJSON(&refreshTokenParameter); err != nil {
+		return
+	}
 
-// 	// token, err := h.authTokenManager.RefreshToken(ctx, refreshTokenParameter.RefreshToken)
-// 	// if err != nil {
-// 	// 	c.Status(http.StatusBadRequest)
-// 	// 	return
-// 	// }
+	// token, err := h.authTokenManager.RefreshToken(ctx, refreshTokenParameter.RefreshToken)
+	// if err != nil {
+	// 	c.Status(http.StatusBadRequest)
+	// 	return
+	// }
 
-// 	// logger.Info("Authorize OK")
-// 	// c.JSON(http.StatusOK, AuthResponse{
-// 	// 	AccessToken: token,
-// 	// })
-// }
+	// logger.Info("Authorize OK")
+	// c.JSON(http.StatusOK, AuthResponse{
+	// 	AccessToken: token,
+	// })
+}
