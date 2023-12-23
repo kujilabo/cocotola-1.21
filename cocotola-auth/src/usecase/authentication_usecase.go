121119c70edb7d7a9c0479c1f4ab5751e75e96aa
pecolynx <pecolynx@gmail.com>
2023-12-24 01:52:18 +0900

    develop

--- cocotola-auth/src/usecase/authentication_usecase.go
+++ cocotola-auth/src/usecase/authentication_usecase.go
@@ -2,7 +2,7 @@ package usecase
 
 import (
 	"context"
-	"fmt"
+	// "fmt"
 
 	"github.com/golang-jwt/jwt"
 
@@ -10,8 +10,8 @@ import (
 	rsuserservice "github.com/kujilabo/redstart/user/service"
 
 	"github.com/kujilabo/cocotola-1.21/cocotola-auth/src/domain"
-	liblog "github.com/kujilabo/cocotola-1.21/lib/log"
-	rsliblog "github.com/kujilabo/redstart/lib/log"
+	// liblog "github.com/kujilabo/cocotola-1.21/lib/log"
+	// rsliblog "github.com/kujilabo/redstart/lib/log"
 )
 
 type AppUserClaims struct {
@@ -37,19 +37,19 @@ func NewAuthentication(rf rsuserservice.RepositoryFactory, signingKey []byte) *A
 }
 
 func (u *Authentication) Authenticate(ctx context.Context, bearerToken string) (*rsuserdomain.AppUserModel, error) {
-	logger := rsliblog.GetLoggerFromContext(ctx, liblog.AppUsecaseLoggerContextKey)
+	// logger := rsliblog.GetLoggerFromContext(ctx, liblog.AppUsecaseLoggerContextKey)
 
 	token, err := jwt.ParseWithClaims(bearerToken, &AppUserClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return u.signingKey, nil
 	})
 	if err != nil {
-		logger.InfoContext(ctx, fmt.Sprintf("invalid token. err: %v", err))
+		// logger.InfoContext(ctx, fmt.Sprintf("invalid token. err: %v", err))
 		return nil, domain.ErrUnauthenticated
 	}
 
 	claims, ok := token.Claims.(*AppUserClaims)
 	if !ok || !token.Valid {
-		logger.InfoContext(ctx, "invalid token")
+		// logger.InfoContext(ctx, "invalid token")
 		return nil, domain.ErrUnauthenticated
 	}
 
